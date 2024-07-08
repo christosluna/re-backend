7508213e157558f06b0d1f68fe1abd47f0915ca1
Roni Christian <rpuno@hyperstacksinc.com>
2024-07-08 12:01:19 +0800

    update port configuration

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'process';
 
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
